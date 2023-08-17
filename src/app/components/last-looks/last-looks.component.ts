812f25c2194cbf9147ee33e19f6d27fb57a4765b
mckiernantim <mckiernantim@gmail.com>
2023-08-17 17:17:59 -0400

    fixes styling in last looks - needs end / start styling

--- src/app/components/last-looks/last-looks.component.ts
+++ src/app/components/last-looks/last-looks.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit  } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Line } from 'src/app/types/Line';
 
 @Component({
@@ -6,46 +6,59 @@ import { Line } from 'src/app/types/Line';
   templateUrl: './last-looks.component.html',
   styleUrls: ['./last-looks.component.css'],
 })
-export class LastLooksComponent implements OnInit{
+export class LastLooksComponent implements OnInit {
   @Input() doc: any;
   pages: [];
   currentPageIndex: number = 0;
-  currentPage: number = 0
+  currentPage: number = 0;
   ngOnInit() {
     this.pages = this.doc.data;
-    alert("firing ")
-    this.processLinesForLastLooks(this.pages)
-    this.updateDisplayedPage()
+    alert('firing ');
+    this.processLinesForLastLooks(this.pages);
+    this.updateDisplayedPage();
   }
   updateDisplayedPage() {
-    this.currentPage = this.pages[this.currentPageIndex]
+    this.currentPage = this.pages[this.currentPageIndex];
   }
   previousPage() {
     if (this.currentPageIndex > 0) {
       this.currentPageIndex--;
-      this.updateDisplayedPage()
+      this.updateDisplayedPage();
     }
   }
-  
+  startSingle = function (barY) {
+    return barY * 1.3 - 44 + 'px';
+  };
+  formatEndY = function (endY) {
+    if (endY > 90) {
+      return endY + 'px';
+    } else return 90 + 'px';
+  };
   nextPage() {
     if (this.currentPageIndex < this.pages.length) {
-      this.currentPageIndex++;  
+      this.currentPageIndex++;
       this.updateDisplayedPage();
-
-}
+    }
   }
- 
+
   processLinesForLastLooks(arr) {
-    for(let page of arr) {
-      page.forEach((line:Line) => {
+    for (let page of arr) {
+      page.forEach((line: Line) => {
+        if (line.bar) {
+          line.calculatedYpos =
+            Number(line.yPos) > 1 ? Number(line.yPos) * 1.3 + 'px' : '0';
+        } else {
+          line.calculatedYpos =
+            Number(line.yPos) > 1 ? Number(line.yPos) * 1.3 + 'px' : '0';
+        }
+
         // Perform your calculations and store the results in each line object
         line.calculatedXpos = Number(line.xPos) * 1.3 + 'px';
-        
-        line.calculatedYpos = Number(line.yPos) > 1 ? Number(line.yPos) * 1.3 + 'px' : '0';
-        line.calculatedEnd = Number(line.endY) > 90 ? Number(line.endY) * 1.3 + 'px' : '90px';
+
+        line.calculatedEnd =
+          Number(line.endY) > 90 ? Number(line.endY) * 1.3 + 'px' : '90px';
         // ... other calculations
       });
     }
   }
 }
-
