0c0ec4c0e93f10a9569ea2bf35f20b9bf753ba6e
mckiernantim <mckiernantim@gmail.com>
2024-07-02 17:20:14 -0400

    adds dashboard component tests- removes dead components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,41 +5,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-
-
 import { LayoutModule } from '@angular/cdk/layout';
-
 // material stuff
 import { MaterialModule } from './modules/material-module/material.module';
 // Firebase
 import { FirebaseModule } from './modules/firebase-module/firebase.module';
-
 import { UploadModule } from './modules/upload-module/upload.module';
-
-
-
 // components
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { DonateComponent } from './components/donate/donate.component';
-
 import { IssueComponent } from './components/issue/issue.component';
-import { ScriptComponent } from './components/script/script.component';
-
-
-import { WarningComponent } from "./components/warning/warning.component"
 import { SharedModule } from './modules/shared-module/shared.module';
 import { DashboardModule } from './modules/dashboard-module/dashboard.module';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ScriptComponent,
         IssueComponent,
         AboutComponent,
         DonateComponent,
-        WarningComponent,
-    ],
+     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
@@ -53,13 +39,22 @@ import { DashboardModule } from './modules/dashboard-module/dashboard.module';
         DashboardModule,
         SharedModule,
     ],
-    
-
-  
     providers: [
         DatePipe,
     ],
-
     bootstrap: [AppComponent]
 })
 export class AppModule {}
+    
+
+  
+
+
+
+
+
+
+
+
+
+
--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,19 +1,40 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
+import { UploadService } from '../../services/upload/upload.service';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
+class MockUploadService {
+  getFile(file: string) {
+    return of(new Blob(['test file content'], { type: 'application/pdf' }));
+  }
+}
+
+class MockRouter {
+  navigate = jest.fn();
+}
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let mockUploadService: MockUploadService;
+  let mockRouter: MockRouter;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DashboardComponent],
+      providers: [
+        { provide: UploadService, useClass: MockUploadService },
+        { provide: Router, useClass: MockRouter }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
+    mockUploadService = TestBed.inject(UploadService);
+    mockRouter = TestBed.inject(Router) as unknown as MockRouter;
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +43,17 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get file name from local storage on init', () => {
+    const localStorageMock = {
+      getItem: jest.fn().mockReturnValue('test-file.pdf')
+    };
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+
+    component.ngOnInit();
+    expect(localStorage.getItem).toHaveBeenCalledWith('name');
+    expect(component.file).toBe('test-file.pdf');
+  });
+
+
 });
