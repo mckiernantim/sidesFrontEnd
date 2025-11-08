981d993273b67a7faa856f48535f144b338dad9e
mckiernantim <mckiernantim@gmail.com>
2025-11-08 14:12:13 -0500

    env for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,29 +6,29 @@
 export const environment = {
   production: false,
   stripe: 'pk_test_51IEIywBojwZRnVT4jdQQwACDdPb6Zy0ceGk09ZXvUWoeseNOakmMrGB5F9aVY73b0VQqwhZD6jCOE74GTGXbV4Tj00ggYYXpjQ',
-  // firebaseConfig:{
-  //   apiKey: "AIzaSyBXD5kQfAS4lrmSJxYAuEUq8sxvXgWmCio",
-  //   authDomain: "scriptthing.firebaseapp.com",
-  //   databaseURL: "https://scriptthing.firebaseio.com",
-  //   projectId: "scriptthing",
-  //   storageBucket: "scriptthing.appspot.com",
-  //   messagingSenderId: "195325163986",
-  //   appId: "1:195325163986:web:e7a70646f087850353300b",
-  //   measurementId: "G-XM3P84P6N7"
-  // },
-  firebaseConfig: {
-    apiKey: "AIzaSyCr0Gemya880xoOnAYWtTcZWssg5Uc2HY0",
-    authDomain: "scriptthing-dev.firebaseapp.com",
-    projectId: "scriptthing-dev",
-    storageBucket: "scriptthing-dev.firebasestorage.app",
-    messagingSenderId: "401150394674",
-    appId: "1:401150394674:web:760ffe3a546b2d01a8d72b",
-    measurementId: "G-1JF7DG5L5H"
+  firebaseConfig:{
+    apiKey: "AIzaSyBXD5kQfAS4lrmSJxYAuEUq8sxvXgWmCio",
+    authDomain: "scriptthing.firebaseapp.com",
+    databaseURL: "https://scriptthing.firebaseio.com",
+    projectId: "scriptthing",
+    storageBucket: "scriptthing.appspot.com",
+    messagingSenderId: "195325163986",
+    appId: "1:195325163986:web:e7a70646f087850353300b",
+    measurementId: "G-XM3P84P6N7"
   },
+  // firebaseConfig: {
+  //   apiKey: "AIzaSyCr0Gemya880xoOnAYWtTcZWssg5Uc2HY0",
+  //   authDomain: "scriptthing-dev.firebaseapp.com",
+  //   projectId: "scriptthing-dev",
+  //   storageBucket: "scriptthing-dev.firebasestorage.app",
+  //   messagingSenderId: "401150394674",
+  //   appId: "1:401150394674:web:760ffe3a546b2d01a8d72b",
+  //   measurementId: "G-1JF7DG5L5H"
+  // },
   url: 'https://sides3-dev-e045a1d9ac46.herokuapp.com',
   // url: 'http://localhost:8080',
   password:"NOTEWORTHY",
-  maintenanceMode: false
+  maintenanceMode: true
 };
 
 // For production - this will be used if environment.prod.ts isn't properly loaded
@@ -45,7 +45,7 @@ export const environmentProd = {
     measurementId: "G-1JF7DG5L5H",
   },
   url: 'https://sides3-dev-e045a1d9ac46.herokuapp.com',
-  maintenanceMode: false
+  maintenanceMode: true 
 };
 
 // Helper function to get the right environment
