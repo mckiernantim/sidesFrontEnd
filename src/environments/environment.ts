e680fc09ab916bcf018b3f3bafce5408a3baa2af
mckiernantim <mckiernantim@gmail.com>
2026-01-26 13:25:20 -0500

    hotfix - updates prod config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,7 +18,7 @@ const prodFirebaseConfig = {
   authDomain: "scriptthing.firebaseapp.com",
   databaseURL: "https://scriptthing.firebaseio.com",
   projectId: "scriptthing",
-  storageBucket: "scriptthing.appspot.com",
+  storageBucket: "scriptthing.firebasestorage.app",
   messagingSenderId: "195325163986",
   appId: "1:195325163986:web:e7a70646f087850353300b",
   measurementId: "G-XM3P84P6N7"
@@ -37,9 +37,9 @@ export const environment = {
 // Fallback for production build (used by environment.prod.ts if not properly loaded)
 export const environmentProd = {
   production: true,
-  stripe: "pk_test_51RJhtgPfZjNtFCgVntmnKxLEbkXUeVlZr5wglNgYmrrXrBM3TUwNDFuTyJhTJAKkO6SJ5Q8eCLTJzCHvI4cfLBqp00Ljc1AZxm",
-  firebaseConfig: devFirebaseConfig,
-  url: 'https://sides3-dev-e045a1d9ac46.herokuapp.com',
+  stripe: 'pk_test_51IEIywBojwZRnVT4jdQQwACDdPb6Zy0ceGk09ZXvUWoeseNOakmMrGB5F9aVY73b0VQqwhZD6jCOE74GTGXbV4Tj00ggYYXpjQ',
+  firebaseConfig: prodFirebaseConfig,
+  url: 'https://sides3.herokuapp.com',
   password: "NOTEWORTHY",
   maintenanceMode: false 
 };
